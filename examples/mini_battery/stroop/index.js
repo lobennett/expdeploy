5151ffe19a3c1e16b06f10f0e170244d522c5d1e
Logan Bennett <bennett.j.logan@gmail.com>
2026-05-15 19:09:17 -0700

    Add examples/mini_battery (flanker + stroop, fixed counterbalance)

---
+++ examples/mini_battery/stroop/index.js
@@ -0,0 +1,24 @@
+import { initJsPsych } from "jspsych";
+import htmlKeyboardResponse from "@jspsych/plugin-html-keyboard-response";
+
+export default function build() {
+  const startedAt = new Date().toISOString();
+  const jsPsych = initJsPsych({
+    on_finish: () => {
+      window.expdeploy.submit({
+        exp_id: "stroop",
+        subject_id: window.expdeploy.subjectId,
+        started_at: startedAt,
+        ended_at: new Date().toISOString(),
+        trials: jsPsych.data.get().values(),
+        status: "finished",
+      }).then(() => {
+        document.body.innerHTML += '<p>Stroop done. Loading next...</p>';
+        setTimeout(() => location.reload(), 500);
+      });
+    },
+  });
+  jsPsych.run([
+    { type: htmlKeyboardResponse, stimulus: "<h1>Stroop</h1><p>Press any key.</p>" },
+  ]);
+}
